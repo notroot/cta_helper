9be9a637c1faed4ae9359a0e5e0874de4cc984e5
Jim Lester <jim@jimlester.net>
2014-12-13 14:45:35 -0600

    New class for buss stop, logic for day/time and formating fixes

--- cta_helper.py
+++ cta_helper.py
@@ -4,6 +4,7 @@ from urllib2 import Request, urlopen, URLError
 import xml.etree.ElementTree as ET
 import ConfigParser
 from datetime import datetime
+from time import gmtime, strftime
 
 # Flask imports
 from flask import Flask, request, session, g, redirect, url_for, \
@@ -24,7 +25,25 @@ api_key = config.get('Options','api_key')
 
 #DEBUG=False
 
-def getTimes(stop, bus=None):
+class BussStop(object):
+	def __init__(self, name, bus, stop_id, direction):
+		self.name = name
+		self.bus = int(bus)
+		self.stop_id = int(stop_id)
+		self.direction = direction
+		self.prdtms = None
+
+		
+	def __repr__(self):
+		return '{} - {}, {}'.format(self.name, self.bus, self.stop_id)
+
+
+def getTimes(buss_stop):
+
+	bus = buss_stop.bus
+	stop = buss_stop.stop_id
+	#app.logger.debug("Getting log for %s, %s" % (bus, stop))
+	
 	request = "http://www.ctabustracker.com/bustime/api/v1/getpredictions?key=%s&rt=%s&stpid=%s" % (api_key,bus,stop)
 
 	try:
@@ -41,7 +60,8 @@ def getTimes(stop, bus=None):
 		#ET.dump(atype)
 		prdtm = atype.find('prdtm').text
 		prdtms.append({'prdtm':prdtm,'minutes':timeTilDepart(prdtm)})
-
+	
+	#app.logger.debug("Got times for %s, %s: %s" % (bus, stop, prdtms))
 	return prdtms
 
 def timeTilDepart(prdtm):
@@ -54,8 +74,7 @@ def timeTilDepart(prdtm):
 
 	now = datetime.now()
 
-	# bad hack right now for EDT
-	c_hour = now.hour - 1
+	c_hour = now.hour
 	c_minute = now.minute
 
 	if ( c_hour == hour):
@@ -75,32 +94,47 @@ def convertToMin(prdtms):
 
 	return mins
 
+def getBusses():
+	week_day = int(strftime("%w"))
+	hour = int(strftime("%H"))
 	
-@app.route('/')
-def show_home():
-	busses=[ {'bus':156,'stop':14461}, {'bus':134,'stop':6711} ]
-	busses=[ {'bus':22,'stop':14439} ]
+	busses = []
 
+	# weekday morning
+	if (week_day > 0 and week_day < 6 and hour > 4 and hour < 12):
+		busses.append(BussStop("Sheridan & Surf", 156, 1076, "South"))
+		busses.append(BussStop("Sheridan & Surf", 134, 1076, "South"))
+		busses.append(BussStop("Diversey & Sheridan", 76, 11037, "West"))
+	# weekday commute
+	elif (week_day > 0 and week_day < 6 and hour > 12 and hour < 19):
+		busses.append(BussStop("Jackson & River", 156, 14461, "North"))
+		busses.append(BussStop("Franklin & Jackson", 134, 6711, "North"))
+		busses.append(BussStop("Diversey & Brown Line", 76, 11028, "East"))
+	# weekday evening and weekend
+	else:	
+		busses.append(BussStop("Clark & Southport", 22, 14439, "South"))
+		busses.append(BussStop("Clack & Diversey", 22, 1917, "North"))
+
+	return busses
+
+@app.route('/')
+def show_home():	
+	current_time = strftime("%I:%M:%S")
+
+	busses = getBusses()
+	
 	results = []
 	for bus_stop in busses:
-		bus = bus_stop['bus']
-		stop = bus_stop['stop']
-		prdtms = getTimes(stop,bus)
+		prdtms = getTimes(bus_stop)
 
-		bus_stop['prdtms'] = prdtms
+		bus_stop.prdtms = prdtms
 		#bus_stop['ttds'] = convertToMin(prdtms)
 
 		results.append(bus_stop)
-		
-		#if (len(prdtms) > 0):
-		#	print "The %s's will arrive at the following time(s):"%bus
-		#	for prdtm in prdtms:
-		#		ttd = timeTilDepart(prdtm)
-		#		print "%s minutes (%s)" % (ttd, prdtm)
-		#else:
-		#	print "No %s's coming anytime soon"%bus
 
-		return render_template('show_main.html', results=results)
+	
+
+	return render_template('show_main.html', current_time=current_time, results=results)
 
 app.wsgi_app = ProxyFix(app.wsgi_app)
 if __name__ == '__main__':
